2d8f1fdc27d1237a72a082f4ed28c82c8acaf0aa
Preston-Too <toopreston@gmail.com>
2021-02-10 16:03:14 +0300

    added views functions

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def about_me(request):
+    return render(request, 'about.html')
